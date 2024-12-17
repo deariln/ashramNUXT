c109ad9bea41cea99dc36d29c5a10c0c7e90732d
deariln <deariln@mail.ru>
2024-12-17 19:43:10 +0300

    answer

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="main-page">
+    <section id="main-page">
         <div id="textpic">
             <!-- <img src="photo/main.jpg" alt="main-logo" id="main-logo"> -->
             <div class="text">
@@ -113,7 +113,7 @@
             </div>
             <div class="button-cab">
                 <button class="but-cab">Подробнее</button>
-              </div>
+            </div>
         </div>
         <div class="cab1">
             <div class="head-cab1">
@@ -139,7 +139,7 @@
                 </div>
             </div>
             <div class="button-cab">
-              <button class="but-cab">Подробнее</button>
+                <button class="but-cab">Подробнее</button>
             </div>
         </div>
     </section>
@@ -160,16 +160,16 @@
                 <div class="button-rules">
                     <NuxtLink to="/rules_one" class="rules-one">
                         Правила аренды
-                   </NuxtLink>
-                   <NuxtLink to="/rules_two" class="rules-two">
-                    Договор офферты
-                   </NuxtLink>
+                    </NuxtLink>
+                    <NuxtLink to="/rules_two" class="rules-two">
+                        Договор офферты
+                    </NuxtLink>
                 </div>
             </div>
         </div>
 
     </section>
-   
+
     <section class="answers-page">
         <div class="h5">
             <h5>Ответы на частые вопросы</h5>
@@ -177,62 +177,153 @@
         <div class="qestion">
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Что входит в стоимость?</p>
+                    <button @click="toggleQestion('qestion')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion'" class="answer">
+                    <ul class="ul-answer">
+                        <li>- рабочего место</li>
+                        <li>- расходные материалы</li>
+                        <li> - оборудование для фото и видео</li>
+                        <li>- чай, кофе, вода, сладости</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Как происходит оплата?</p>
+                    <button @click="toggleQestion('qestion2')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion2'" class="answer">
+                    <ul class="ul-answer">
+                        <li> Оплата происходит сразу же после работы переводом или наличными</li>
+                        <li>Аренда гримерного стола: 125 ₽ за 30 минут</li>
+                        <li>Аренда кабинета "Стандарт": 150 ₽ за 30 минут</li>
+                        <li>Аренда кабинета "Комфорт": 175 ₽ за 30 минут</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Есть ли администратор?</p>
+                    <button @click="toggleQestion('qestion3')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion3'" class="answer">
+                    Обращаем ваше внимание, что в пространстве вы находитесь одни. Администратор работает удаленно, и вы
+                    можете написать ему по любому вопросу
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Как забронировать место?</p>
+                    <button @click="toggleQestion('qestion4')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion4'" class="answer">
+                    <ul class="ul-answer">
+                        <li> Аренда рабочих мест осуществляется по предварительному бронированию без предоплаты до 5ч
+                            аренды. От 5
+                            часов - предоплата в размере 100%.
+                            Вы можете совершить бронирование любым из удобных для вас способов:</li>
+                        <li>1. С помощью сообщения в wa +7 993 768 63 15</li>
+                        <li>2. На сайте через форму онлайн-записи</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Как перенести или отменить бронь?</p>
+                    <button @click="toggleQestion('qestion5')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion5'" class="answer">
+                    <ul class="ul-answer">
+                        <li>Вы можете перенести/отменить бронь любым из удобных для Вас способов:</li>
+                        <li>1. С помощью сообщения в wa +7 993 768 63 15</li>
+                        <li>2. На сайте через форму онлайн-записи</li>
+                        <li>В случае отмены брони:</li>
+                        <li>— Отмена или перенос менее чем за 24ч – оплата 100%</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Какие расходные материалы входят в стоимость?</p>
+                    <button @click="toggleQestion('qestion6')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion6'" class="answer">
+                    <ul class="ul-answer">
+                        <li>Мы предоставляем расходные материалы бесплатно:</li>
+                        <li>- ватные диски,</li>
+                        <li>- ватные палочки,</li>
+                        <li>- одноразовые простыни,</li>
+                        <li>- бумажные полотенца,</li>
+                        <li>- влажные салфетки,</li>
+                        <li>- антисептик.</li>
+                        <li>Также у нас можно приобрести:</li>
+                        <li>- перчатки 30 руб/шт</li>
+                        <li>- крафт пакет 10 руб/шт</li>
+                        <li>- патчи 10 руб/шт</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Можно ли занять место раньше?</p>
+                    <button @click="toggleQestion('qestion7')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion7'" class="answer">
+                    Только по согласованию с администратором, даже если место свободно. Вы можете связаться с
+                    администратором в чате WA +7 993 768 63 15
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Есть ли абонементы?</p>
+                    <button @click="toggleQestion('qestion8')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
+                </div>
+                <div v-if="isOpen === 'qestion8'" class="answer">
+                    <ul class="ul-answer">
+                        <li>Абонемент в гримерный зал:</li>
+                        <li>20 часов - 4800 руб</li>
+                        <li>40 часов - 9200 руб</li>
+                        <li>Абонемент в кабинет "Стандарт":</li>
+                        <li>20 часов - 5800 руб</li>
+                        <li>40 часов - 11200 руб</li>
+                        <li>Абонемент в кабинет "Комфорт":</li>
+                        <li>20 часов - 6800 руб</li>
+                        <li>40 часов - 13200 руб</li>
+                    </ul>
                 </div>
             </div>
             <div class="window">
                 <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
                     <p>Можно ли продлить бронь, если не успеваешь закончить?</p>
+                    <button @click="toggleQestion('qestion9')" class="qwe-butt">
+                        <img class="img-qestions" src="/photo/arrow.png" alt="">
+                    </button>
                 </div>
-            </div>
-            <div class="window">
-                <div class="icon">
-                    <img class="img-qestions" src="/photo/arrow.png" alt="">
-                    <p>Есть ли парковка?</p>
+                <div v-if="isOpen === 'qestion9'" class="answer">
+                    <ul class="ul-answer">
+                        <li>Продлить бронь возможно только по предварительному согласованию с администратором, в ином
+                            случае,
+                            рабочее место необходимо освободить для работы следующего мастера.</li>
+                        <li>— Задержка рабочего места более 10 минут, оплачивается по тарифу аренды 30/60 мин</li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -267,11 +358,18 @@
             </div> -->
         </div>
     </section>
-
 </template>
 
 <script setup lang="ts">
+const isOpen = ref(null)
+
+const toggleQestion = (qestion:any) => {
+    if (isOpen.value === qestion) {
+        isOpen.value = null
+    } else {
+        isOpen.value = qestion
+    }
+}
 </script>
 
-<style>
-</style>
+<style></style>
