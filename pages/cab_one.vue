a7e7d81ea2d850570902807102078df1359006ef
deariln <deariln@mail.ru>
2024-12-19 19:19:48 +0300

    cab

--- pages/cab_one.vue
+++ pages/cab_one.vue
@@ -2,19 +2,32 @@
     <h1 class="h1">
         Гримерный стол
     </h1>
-    <Swiper :navigation="true" :modules="modules" class="mySwiper">
-    <SwiperSlide><img src="/public/photo/cab-one1.jpeg" alt="#"></SwiperSlide>
-    <SwiperSlide><img src="/public/photo/cab-one2.jpeg" alt="#"></SwiperSlide>
-    <SwiperSlide><img src="/public/photo/cab-one3.jpeg" alt="#"></SwiperSlide>
-    <SwiperSlide><img src="/public/photo/cab-one4.jpeg" alt="#"></SwiperSlide>
-    <SwiperSlide><img src="/public/photo/cab-one5.jpeg" alt="#"></SwiperSlide>
-    <SwiperSlide><img src="/public/photo/cab-one6.jpeg" alt="#"></SwiperSlide>
-  </Swiper>
+    <div class="box">
+        <Swiper :navigation="true" :modules="modules" class="mySwiper">
+            <SwiperSlide><img src="/public/photo/cab-one1.jpeg" alt="#"></SwiperSlide>
+            <SwiperSlide><img src="/public/photo/cab-one2.jpeg" alt="#"></SwiperSlide>
+            <SwiperSlide><img src="/public/photo/cab-one3.jpeg" alt="#"></SwiperSlide>
+            <SwiperSlide><img src="/public/photo/cab-one4.jpeg" alt="#"></SwiperSlide>
+            <SwiperSlide><img src="/public/photo/cab-one5.jpeg" alt="#"></SwiperSlide>
+            <SwiperSlide><img src="/public/photo/cab-one6.jpeg" alt="#"></SwiperSlide>
+        </Swiper>
+        <div class="div-ul">
+            <ul class="ul-box">
+                <li>2 гримерных места</li>
+                <li>Расходные материалы входят в стоимость</li>
+                <li>В гримерке стоит кулер</li>
+                <li>Чай, кофе, вода, сладости</li>
+                <li>Аренда хранения материалов</li>
+                <li>Оборудование для фото и видео</li>
+            </ul>
+            <button class="box-button">На главную</button>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-    layout:'cab'
+    layout: 'cab'
 })
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -34,8 +47,33 @@ const modules = [Navigation]
     padding: 20px;
     background-color: gainsboro;
 }
+
 .mySwiper {
     height: 700px;
     width: 700px;
+    margin: 0;
+}
+
+.box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 30px;
+    gap: 50px;
+}
+
+.box-button {
+    color: rgb(70, 70, 70);
+    background: transparent;
+    border-radius: 20px;
+    padding: 5px;
+    font-size: 18px;
+    width: 200px;
+    margin: 10px;
+}
+
+.ul-box {
+    font-size: 20px;
+    list-style-type: disc;
 }
 </style>
