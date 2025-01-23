804b215cc1a9a7c8713ade78b515e1bcee915ec5
deariln <deariln@mail.ru>
2025-01-23 20:17:38 +0300

    000

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,11 +3,11 @@
     <nav>
       <img src="/photo/logo.PNG" alt="logo" id="logo">
       <ul id="navbar">
-          <li><a href="#service">Услуги</a></li>
           <li><a href="#places">Рабочие места</a></li>
           <li><a href="#rules">Правила</a></li>
-          <li><a href="#qestion">Ответы на вопросы</a></li>
           <li><a href="#contacts">Контакты</a></li>
+          <li><a href="#qestion">Ответы на вопросы</a></li>
+          
       </ul>
       <!-- <div class="button-contact-div">
           <button class="button-contact">Связаться с нами</button>
